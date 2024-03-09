2fde13432b913d9f9a88b0dbdbba74a31cefd398
KevinatorYu <kevinator.yu@hotmail.com>
2024-03-08 19:20:46 -0800

    edited comments on ddl.py

--- DDL.py
+++ DDL.py
@@ -1,10 +1,13 @@
+## BE SURE TO HAVE Tables.txt DOWNLOADED IN THE SAME FOLDER AS THIS .PY FILE
+## It contains the CREATE TABLE statements that this code will need to read in and generate the .sql file with
+## WITHOUT THE .txt FILE THE CODE WILL NOT WORK
+
 import pandas as pd
 
 file_paths = ["data/tidied_population_estimates.csv","data/tidied_industry_mapping.csv","data/tidied_employment_estimates.csv","data/tidied_emissions_with_key.csv"]
 tables = ['province', 'maping', 'employees', 'fuel_emissions']
-# Function to generate INSERT statements for the Industry table
 
-
+# Function to generate INSERT statements for the Mapping table
 def generate_insert_maping_statements(data):
     insert_statements = []
     for index, row in data.iterrows():
@@ -15,7 +18,6 @@ def generate_insert_maping_statements(data):
 # Function to generate INSERT statements for the Employees table
 def generate_insert_employees_statements(data):
     insert_statements = []
-    #data = pd.melt(data, id_vars=['naics_industry','province'], var_name='Year', value_name='Value')
     for index, row in data.iterrows():
         insert_statement = f"insert into Industry values ('{row[0]}','{row[1]}', {row[2]}, {row[3]});"
         insert_statements.append(insert_statement)
@@ -38,7 +40,8 @@ def generate_insert_fuel_emissions_statements(data):
     return insert_statements
 
 inserts = []
-# Example usage
+
+# Main
 if __name__ == "__main__":
 
     # Loop through each file and generate INSERT statements
@@ -50,7 +53,6 @@ if __name__ == "__main__":
         insert_statements = None
         
         # Generate INSERT statements based on table name
-
         if table_name == 'employees':
             data = pd.read_csv(file_path)
             insert_statements = generate_insert_employees_statements(data)
@@ -65,10 +67,7 @@ if __name__ == "__main__":
             insert_statements = generate_insert_fuel_emissions_statements(data)
             inserts.append(insert_statements)
 
-       # print(insert_statements)
-        # Execute the INSERT statements using your database connection
-        # db_connection = your_database_connection_function()
-        # execute_insert_statements(insert_statements, db_connection)
+    # Read in Tables.txt, contains all the CREATE TABLE Statements
     with open('tables.txt', 'r') as file:
         existing_content = file.read()
 
